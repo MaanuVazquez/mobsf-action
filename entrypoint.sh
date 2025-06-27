a2521db4eb26c825dff0b94877cbc7b4563fbab9
Emmanuel Vazquez <maanuvazquez@gmail.com>
2025-06-27 13:25:55 -0300

    chore: update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,19 +26,18 @@ export MOBSF_API_KEY="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64)"
 export MOBSF_URL="localhost:8000"
 
 cd /home/mobsf/Mobile-Security-Framework-MobSF
-python3 manage.py makemigrations && \
-python3 manage.py makemigrations StaticAnalyzer && \
-python3 manage.py migrate
+python3 manage.py makemigrations 2&>> manage.out && \
+python3 manage.py makemigrations StaticAnalyzer 2&>> manage.out && \
+python3 manage.py migrate 2&>> manage.out
 set +e
-python3 manage.py createsuperuser --noinput --email ""
+python3 manage.py createsuperuser --noinput --email "" 2&>> manage.out
 set -e
-python3 manage.py create_roles
-
-exec gunicorn -b 0.0.0.0:8000 "mobsf.MobSF.wsgi:application" --workers=1 --threads=10 --timeout=3600 \
-    --worker-tmp-dir=/dev/shm --log-level=citical --log-file=- --access-logfile=- --error-logfile=- --capture-output
+python3 manage.py create_roles 2&>> manage.out
+gunicorn -b 127.0.0.1:8000 "mobsf.MobSF.wsgi:application" --workers=1 --threads=10 --timeout=3600 \
+    --worker-tmp-dir=/dev/shm --log-level=citical --log-file=- --access-logfile=- --error-logfile=- --capture-output &
 
 # Wait to start MobSF
-sleep 2
+sleep 10
 
 cd $GITHUB_WORKSPACE
 
